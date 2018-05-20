d277f52f3585b024bebb70136981742a12255288
Jordan <tjordanwells@gmail.com>
2018-05-20 09:12:49 -0400

    formated html / linked javascript and jquery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,6 @@
 var apiKey = "JNVSpiNfpdWuvNggqkr0qeNnZwAPWYpA";
+
+var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=" + apiKey + "&limit=10";
+
+var search = $("#search").val();
+
